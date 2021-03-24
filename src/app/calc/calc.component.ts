1b256ddcacb8a1801c7e6681322c55ace16b0b6f
Rituraj <riturajgujare01@gmail.com>
2021-03-24 09:08:30 +0530

    changed addition to multiplication

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `
     Enter first number: <input type="number" #no1/><br />
     Enter Second Number: <input type="number" #no2/><br />
-    <button (click)="calculate('+', no1.value, no2.value)">Add</button>
+    <button (click)="calculate('*', no1.value, no2.value)">Add</button>
     <button (click)="calculate('-', no1.value, no2.value)">Sub</button> <br />
     {{result}}
 
@@ -16,8 +16,8 @@ import { Component, OnInit } from '@angular/core';
 export class CalcComponent{
   result: number;
   calculate(op:String, no1: number, no2:number){
-    if(op=='+'){
-      this.result= Number(no1)+ Number(no2);
+    if(op=='*'){
+      this.result= Number(no1)* Number(no2);
     }
     else{
       this.result=Number(no1)-Number(no2);
